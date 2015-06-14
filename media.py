8500e950febabd47e8034f3e3b6280009621652e
Trish Whetzel <plwhetzel@gmail.com>
2015-06-14 10:50:38 -0700

    fix formatting issues

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
 import webbrowser
 
+
 class Movie():
+
     """This class provides a way to store movie related information"""
-    
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_rating):
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, movie_rating):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -14,5 +17,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
-    
